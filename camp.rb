6ecce65f4f5b46afccbd817b8b542b3f488b26f9
Furkan ERDEM <ffrknerdm@gmail.com>
2023-04-21 15:46:12 +0000

    add models

--- camp.rb
+++ camp.rb
@@ -1,26 +1,47 @@
-require 'camping'
+require "camping"
 Camping.goes :Nuts
 
-module Nuts::Controllers
-    class Index < R '/'
-        def get
-            @time = Time.now
-            render :sundial
-        end
+module Nuts::Models
+  class Page < Base
+  end
+
+  class BasicFields < V 1.0
+    def self.up
+      create_table Page.table_name do |t|
+        t.string :title
+        t.text :content
+        t.timestamps
+      end
     end
+
+    def self.down
+      drop_table Page.table_name
+    end
+  end
+end
+
+module Nuts::Controllers
+  class Index < R "/"
+    def get
+      @pages = Page.find :all
+      render :sundial
+    end
+  end
 end
 
 module Nuts::Views
-    def layout
-      html do
-        head do
-          title { "Nuts And GORP" }
-        end
-        body { self << yield }
-      end
+  def layout
+    html do
+      head { title { "Nuts And GORP" } }
+      body { self << yield }
     end
-  
-    def sundial
-      p "The current time is: #{@time}"
-    end
+  end
+
+  def sundial
+    p "The current time is: #{@time}"
+  end
+end
+
+def Nuts.create
+  Nuts::Models.create_schema
 end
